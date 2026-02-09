84ac3ee524c412900a69f3c9f972ba9b25ed13b0
Meshack <45800447+Meshack-Kinyua@users.noreply.github.com>
2026-02-09 13:42:41 -0500

    Dashboard

---
+++ components/FactoryDashboard.tsx
@@ -0,0 +1,13 @@
+export default function FactoryDashboard() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl">Virtual Factory</h1>
+      <ul className="mt-6 space-y-2">
+        <li>✔ Order Queues</li>
+        <li>✔ Manufacturing Status</li>
+        <li>✔ Cost Estimation</li>
+        <li>✔ Logistics</li>
+      </ul>
+    </div>
+  )
+}
